3a830e21f77acfb0a35ced148bb0bdca673a5f9b
Fran <fmjerez@icloud.com>
2020-10-28 12:22:00 +0100

    MongoDB connected to app

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const dBConnect = require("./config/db");
 
 //Crear servidor
 const app = express();
+
+//Conectar a la DB
+dBConnect();
 
 //Crear Puerto de la App
 const PORT = process.env.PORT || 4000;
