8d8fac8241b810460cddcad10ea96b8ca2e7ddd7
Fran <fmjerez@icloud.com>
2020-10-28 11:21:03 +0100

    Port runing

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log("index.js");
+const express = require("express");
+
+//Crear servidor
+const app = express();
+
+//Crear Puerto de la App
+const PORT = process.env.PORT || 4000;
+
+//Arrancar la app
+app.listen(PORT, () => {
+  console.log(`Server is Running in port ${PORT}`);
+});
