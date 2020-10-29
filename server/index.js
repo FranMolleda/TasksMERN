4ee7ed480c247bb5b00566414d5495e845e9fa4f
Fran <fmjerez@icloud.com>
2020-10-29 09:06:34 +0100

    User router

--- server/index.js
+++ server/index.js
@@ -7,8 +7,14 @@ const app = express();
 //Conectar a la DB
 dBConnect();
 
+// Habilitar express.json
+app.use(express.json({ extended: true }));
+
 //Crear Puerto de la App
 const PORT = process.env.PORT || 4000;
+
+//Importar rutas
+app.use("/api/users", require("./routes/userRoute"));
 
 //Arrancar la app
 app.listen(PORT, () => {
