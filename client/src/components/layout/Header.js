5c9c8d66b3067c41053b4a199c098f6023bfbeef
Fran <fmjerez@icloud.com>
2021-01-27 20:02:22 +0100

    prueba

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
   const authContext = useContext(AuthContext);
   const { user, authUser, logOut } = authContext;
 
+  //UseEffect
   useEffect(() => {
     authUser();
     // eslint-disable-next-line
